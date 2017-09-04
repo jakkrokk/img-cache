da00381add108ff37ee9e45b1590c8ff5ce251e8
jakkrokk <neo.inspiration@gmail.com>
2017-09-04 19:37:37 +0900

    V0.9

---
+++ include/Config.Class.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
+
+Class Config {
+
+	const IMG_SAVE_DIR = __DIR__.'/../webroot/images/';
+
+}
---
+++ include/ImgCache.Class.php
@@ -0,0 +1,121 @@
+<?php
+Class ImgCache extends Config{
+
+	const ALLOW_IMAGE_TYPE = [IMAGETYPE_GIF,IMAGETYPE_JPEG,IMAGETYPE_PNG,IMAGETYPE_BMP];
+
+
+	/**
+	 *
+	 *
+	 * @author jakkrokk
+	 * @copyright jakkrokk
+	 */
+	public function __construct() {
+		$this->domain = $_SERVER['HTTP_HOST'];
+	}
+
+
+	/**
+	 *
+	 *
+	 * @author jakkrokk
+	 * @copyright jakkrokk
+	 */
+	public function attach() {
+		//Allow uid=string name=string AND bin=binary
+		if (isset($_POST['uid']) && isset($_POST['name']) && isset($_POST['bin'])) {
+			$f = $this->createFileName($_POST['uid'],$_POST['name']);
+			//Tmp insert for checking mime.
+			file_put_contents(parent::IMG_SAVE_DIR.$f,$_POST['bin']);
+
+			//Check file is image.
+			if ($this->isImage(parent::IMG_SAVE_DIR.$f)) {
+				//If image then deflate.
+				//$bin = gzdeflate($_POST['bin']);
+				//file_put_contents(parent::IMG_SAVE_DIR.$f,$bin);
+				echo "{$this->domain}/images/{$f}";
+				die;
+			} else {
+				//If non image then unlink.
+				unlink(parent::IMG_SAVE_DIR.$f);
+			}
+		}
+		echo 0;
+	}
+
+
+	/**
+	 *
+	 *
+	 * @author jakkrokk
+	 * @copyright jakkrokk
+	 */
+	public function detach($f) {
+		if (file_exists(parent::IMG_SAVE_DIR.$f)) {
+			unlink(parent::IMG_SAVE_DIR.$f);
+			echo 1;
+			die;
+		}
+		echo 0;
+	}
+
+
+	/**
+	 *
+	 *
+	 * @author jakkrokk
+	 * @copyright jakkrokk
+	 */
+	private function createFileName($uid,$fname) {
+		//Create filename.
+		$c = explode('.',$fname);
+		$d = md5(date('U')).$uid;
+		return $d.'.'.end($c);
+	}
+
+
+	/**
+	 *
+	 *
+	 * @author jakkrokk
+	 * @copyright jakkrokk
+	 */
+	public function test() {
+		$img = file_get_contents('/var/www/html/img.sn.taidakyo.com/p.png');
+
+		//POSTデータ
+		$data = [
+			'uid' => '1',
+			'name' => 'p.png',
+			'bin' => $img
+		];
+		$data = http_build_query($data,'','&');
+
+		//header
+		$header = [
+			'Content-Type: application/x-www-form-urlencoded',
+			'Content-Length: '.strlen($data)
+		];
+		$context = [
+			'http' => [
+				'method' => 'POST',
+				'header'  => implode("\r\n", $header),
+				'content' => $data
+			]
+		];
+
+		$url = "http://img.sn.taidakyo.com/attach.php";
+		echo file_get_contents($url,false,stream_context_create($context));
+	}
+
+
+	/**
+	 *
+	 *
+	 * @author jakkrokk
+	 * @copyright jakkrokk
+	 */
+	private function isImage($path) {
+		return in_array(exif_imagetype($path),self::ALLOW_IMAGE_TYPE);
+	}
+}
